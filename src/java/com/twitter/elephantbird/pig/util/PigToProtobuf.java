c971d69e835f0f770cce59592ceed3654185e48a
Vikram Oberoi <voberoi@gmail.com>
2010-04-21 18:58:14 -0700

    Support for enum type serialization + some documentation.

--- src/java/com/twitter/elephantbird/pig/util/PigToProtobuf.java
+++ src/java/com/twitter/elephantbird/pig/util/PigToProtobuf.java
@@ -1,29 +1,16 @@
 package com.twitter.elephantbird.pig.util;
 
 import java.util.List;
-import java.util.Map;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.protobuf.Descriptors.Descriptor;
-import com.google.protobuf.Descriptors.EnumValueDescriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.google.protobuf.Message;
 import com.google.protobuf.Message.Builder;
-import com.twitter.data.proto.Misc.CountedMap;
 import com.twitter.elephantbird.util.Protobufs;
-import com.twitter.elephantbird.util.TypeRef;
 import org.apache.pig.backend.executionengine.ExecException;
-import org.apache.pig.data.BagFactory;
 import org.apache.pig.data.DataBag;
-import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
-import org.apache.pig.data.TupleFactory;
-import org.apache.pig.impl.logicalLayer.FrontendException;
-import org.apache.pig.impl.logicalLayer.schema.Schema;
-import org.apache.pig.impl.logicalLayer.schema.Schema.FieldSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -59,17 +46,17 @@ public class PigToProtobuf {
 		LOG.warn("Could not convert tuple field " + tupleField + " to field with descriptor " + fieldDescriptor);
 		continue;
 	  }
-	  
+
 	  if (tupleField != null) {
 		if (fieldDescriptor.isRepeated()) { 
 		  // Repeated fields are set with Lists containing objects of the fields' Java type.
-		  builder.setField(fieldDescriptor, dataBagToProtobufList(builder, fieldDescriptor, (DataBag)tupleField));
+		  builder.setField(fieldDescriptor, dataBagToRepeatedField(builder, fieldDescriptor, (DataBag)tupleField));
 		} else {
 		  if (fieldDescriptor.getType() == FieldDescriptor.Type.MESSAGE) {
 			Builder nestedMessageBuilder = builder.newBuilderForField(fieldDescriptor);
 			builder.setField(fieldDescriptor, tupleToMessage((Builder)nestedMessageBuilder, (Tuple)tupleField));
 		  } else {
-			builder.setField(fieldDescriptor, tupleField);
+			builder.setField(fieldDescriptor, tupleFieldToSingleField(fieldDescriptor, tupleField));
 		  }
 		}
 	  }
@@ -78,7 +65,17 @@ public class PigToProtobuf {
 	return builder.build();
   }
 
-  public List dataBagToProtobufList(Builder containingMessageBuilder, FieldDescriptor fieldDescriptor, DataBag bag) {
+  /**
+   * Converts a DataBag into a List of objects with the type in the given FieldDescriptor. DataBags
+   * don't map cleanly to repeated protobuf types, so each Tuple has to be unwrapped (by taking the
+   * first element if the type is primitive or by converting the Tuple to a Message if the type is
+   * MESSAGE), and the contents have to be appended to a List.
+   * @param containingMessageBuilder a Message builder for the Message that contains this repeated field
+   * @param fieldDescriptor a FieldDescriptor for this repeated field
+   * @param bag the DataBag being serialized
+   * @breturn a protobuf-friendly List of fieldDescriptor-type objects
+   */
+  public List dataBagToRepeatedField(Builder containingMessageBuilder, FieldDescriptor fieldDescriptor, DataBag bag) {
 	ArrayList bagContents = new ArrayList((int)bag.size());
 	Iterator<Tuple> bagIter = bag.iterator();
 
@@ -89,7 +86,7 @@ public class PigToProtobuf {
 		bagContents.add(tupleToMessage((Builder)nestedMessageBuilder, tuple));
 	  } else {
 		try {
-		  bagContents.add(tuple.get(0));
+		  bagContents.add(tupleFieldToSingleField(fieldDescriptor, tuple.get(0)));
 		} catch (ExecException e) {
 		  LOG.warn("Could not add a value for repeated field with descriptor " + fieldDescriptor);
 		}		
@@ -97,5 +94,21 @@ public class PigToProtobuf {
 	}
 	  
 	return bagContents;
+  }
+
+  /**
+   * Converts a tupleField string to its corresponding protobuf enum type if necessary, otherwise
+   * returns the tupleField as is.
+   * @param fieldDescriptor the FieldDescriptor for the given tuple field
+   * @param tupleField the tupleField being converted to a protobuf field
+   * @return the protobuf type for the given tupleField. This will be the tupleField itself unless it's an enum, in which case this will return the enum type for the field.
+   */
+  public Object tupleFieldToSingleField(FieldDescriptor fieldDescriptor, Object tupleField) {
+	if (fieldDescriptor.getType() == FieldDescriptor.Type.ENUM) {
+	  // Convert tupleField to the enum value.
+	  return fieldDescriptor.getEnumType().findValueByName((String)tupleField);
+	} else {
+	  return tupleField;
+	}
   }
 }
