012d0778d3cb55e63068c65bf4d08ad65afbf82c
gerritjvv <gvanvuuren@gvanvuuren-Latitude-E4200.(none)>
2010-11-12 13:01:48 +0000

    Fix bug in LzoTextLoader: the nextKeyValue() method should always be checked for true on the RecordReader before calling getCurrentValue.

--- src/java/com/twitter/elephantbird/pig/load/LzoTextLoader.java
+++ src/java/com/twitter/elephantbird/pig/load/LzoTextLoader.java
@@ -1,7 +1,9 @@
 package com.twitter.elephantbird.pig.load;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.pig.PigException;
 import org.apache.pig.backend.executionengine.ExecException;
@@ -14,44 +16,66 @@ import org.slf4j.LoggerFactory;
 import com.twitter.elephantbird.mapreduce.input.LzoTextInputFormat;
 
 /**
- * Load the LZO file line by line, passing each line as a single-field Tuple to Pig.
+ * Load the LZO file line by line, passing each line as a single-field Tuple to
+ * Pig.
  */
 public class LzoTextLoader extends LzoBaseLoadFunc {
-  private static final Logger LOG = LoggerFactory.getLogger(LzoTextLoader.class);
+	private static final Logger LOG = LoggerFactory
+			.getLogger(LzoTextLoader.class);
 
-  private static final TupleFactory tupleFactory_ = TupleFactory.getInstance();
-  protected enum LzoTextLoaderCounters { LinesRead }
+	private static final TupleFactory tupleFactory_ = TupleFactory
+			.getInstance();
 
-  public LzoTextLoader() {
-  }
+	protected enum LzoTextLoaderCounters {
+		LinesRead
+	}
 
-  /**
-   * Return every non-null line as a single-element tuple to Pig.
-   */
-  @Override
-  public Tuple getNext() throws IOException {
-	  if (reader_ == null) {
-		  return null;
-	  }
-	  Tuple t = null;
-	  try {
-		  Object line = reader_.getCurrentValue();
-		  if (line != null) {
-			  incrCounter(LzoTextLoaderCounters.LinesRead, 1L);
-			  t = tupleFactory_.newTuple(new DataByteArray(line.toString().getBytes()));
-		  }
-	  } catch (InterruptedException e) {
-		  int errCode = 6018;
-		  String errMsg = "Error while reading input";
-		  throw new ExecException(errMsg, errCode,
-				  PigException.REMOTE_ENVIRONMENT, e);
-	  }
-	  return t;
-  }
+	private ArrayList<Object> mProtoTuple = null;
 
-  @Override
-  public InputFormat getInputFormat() {
-	  return new LzoTextInputFormat();
-  }
+	public LzoTextLoader() {
+	}
+
+	/**
+	 * Return every non-null line as a single-element tuple to Pig.
+	 */
+	@Override
+	public Tuple getNext() throws IOException {
+		if (reader_ == null) {
+			return null;
+		}
+
+		mProtoTuple = new ArrayList<Object>();
+
+		Tuple t = null;
+		try {
+
+			if (reader_.nextKeyValue()) {
+
+				Text line = (Text) reader_.getCurrentValue();
+
+				if (line != null) {
+					incrCounter(LzoTextLoaderCounters.LinesRead, 1L);
+
+					mProtoTuple.add(new DataByteArray(line.getBytes(), 0, line
+							.getLength()));
+
+					t = tupleFactory_.newTupleNoCopy(mProtoTuple);
+
+				}
+			}
+		} catch (InterruptedException e) {
+			int errCode = 6018;
+			String errMsg = "Error while reading input";
+			throw new ExecException(errMsg, errCode,
+					PigException.REMOTE_ENVIRONMENT, e);
+		}
+		return t;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public InputFormat getInputFormat() {
+		return new LzoTextInputFormat();
+	}
 
 }
