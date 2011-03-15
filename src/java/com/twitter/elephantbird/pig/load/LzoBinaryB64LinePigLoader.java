841bacd0f6ae364ba3bac8d9d27053c13d918659
Raghu Angadi <raghu@twitter.com>
2011-03-15 15:11:42 -0700

    LzoB64LinePig Loaders : loader did not stop at the end of a split. also, while skipping the first record, skip past first newline, not the first good record. moved most of the implementation to common class for Protobuf and Thrift.

---
+++ src/java/com/twitter/elephantbird/pig/load/LzoBinaryB64LinePigLoader.java
@@ -0,0 +1,74 @@
+package com.twitter.elephantbird.pig.load;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.impl.util.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.twitter.elephantbird.mapreduce.io.BinaryConverter;
+
+/**
+ * This is the base class for all base64 encoded, line-oriented
+ * pig loaders for protocol buffers, Thrift etc.
+ * Data is expected to be one base64 encoded serialized object per line.
+ */
+
+public abstract class LzoBinaryB64LinePigLoader extends LzoBaseLoadFunc {
+  private static final Logger LOG = LoggerFactory.getLogger(LzoBinaryB64LinePigLoader.class);
+
+  private BinaryConverter<Tuple> tupleConverter_ = null;
+  private final Base64 base64_ = new Base64();
+
+  private static final Charset UTF8 = Charset.forName("UTF-8");
+  private static final byte RECORD_DELIMITER = (byte)'\n';
+
+  private Pair<String, String> linesRead;
+  private Pair<String, String> recordsRead;
+  private Pair<String, String> errors;
+
+  /**
+   * className is used for naming counters.
+   */
+  protected void init(String className, BinaryConverter<Tuple> tupleConverter) {
+    tupleConverter_ = tupleConverter;
+    String group = "LzoB64Lines of " + className;
+    linesRead = new Pair<String, String>(group, "Lines Read");
+    recordsRead = new Pair<String, String>(group, "Records Read");
+    errors = new Pair<String, String>(group, "Errors");
+  }
+
+  public void skipToNextSyncPoint(boolean atFirstRecord) throws IOException {
+    // Since we are not block aligned we throw away the first (mostly partial)
+    // line of each split and count on a different
+    // instance to read it. The only split this doesn't work for is the first.
+    if (!atFirstRecord && verifyStream()) {
+      is_.readLine(UTF8, RECORD_DELIMITER);
+      // don't increment linesRead either.
+    }
+  }
+
+  /**
+   * Return every non-null line as a single-element tuple to Pig.
+   */
+  public Tuple getNext() throws IOException {
+    String line;
+    Tuple t = null;
+
+    while (verifyStream() && (line = is_.readLine(UTF8, RECORD_DELIMITER)) != null) {
+      incrCounter(linesRead, 1L);
+      t = tupleConverter_.fromBytes(base64_.decode(line.getBytes(UTF8)));
+      if (t != null) {
+        incrCounter(recordsRead, 1L);
+        break;
+      } else {
+        incrCounter(errors, 1L);
+      }
+    }
+
+    return t;
+  }
+}
--- src/java/com/twitter/elephantbird/pig/load/LzoThriftB64LinePigLoader.java
+++ src/java/com/twitter/elephantbird/pig/load/LzoThriftB64LinePigLoader.java
@@ -1,91 +1,57 @@
 package com.twitter.elephantbird.pig.load;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.pig.ExecType;
 import org.apache.pig.backend.datastorage.DataStorage;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.impl.logicalLayer.schema.Schema;
-import org.apache.pig.impl.util.Pair;
 import org.apache.thrift.TBase;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.twitter.elephantbird.mapreduce.io.BinaryConverter;
 import com.twitter.elephantbird.mapreduce.io.ThriftConverter;
 import com.twitter.elephantbird.pig.util.ThriftToPig;
 import com.twitter.elephantbird.util.ThriftUtils;
 import com.twitter.elephantbird.util.TypeRef;
 
-public class LzoThriftB64LinePigLoader<M extends TBase<?, ?>> extends LzoBaseLoadFunc {
+/**
+ * Loader for LZO files with line-oriented base64 encoded Thrift objects.
+ */
+public class LzoThriftB64LinePigLoader<M extends TBase<?, ?>> extends LzoBinaryB64LinePigLoader {
   private static final Logger LOG = LoggerFactory.getLogger(LzoThriftB64LinePigLoader.class);
 
   private final TypeRef<M> typeRef_;
-  private final ThriftConverter<M> converter_;
-  private final Base64 base64_ = new Base64();
+  private ThriftConverter<M> converter_;
   private final ThriftToPig<M> thriftToPig_;
-
-  private static final Charset UTF8 = Charset.forName("UTF-8");
-  private static final byte RECORD_DELIMITER = (byte)'\n';
-
-  private Pair<String, String> linesRead;
-  private Pair<String, String> thriftStructsRead;
-  private Pair<String, String> thriftErrors;
 
   public LzoThriftB64LinePigLoader(String thriftClassName) {
     typeRef_ = ThriftUtils.getTypeRef(thriftClassName);
     converter_ = ThriftConverter.newInstance(typeRef_);
     thriftToPig_ =  ThriftToPig.newInstance(typeRef_);
 
-    String group = "LzoB64Lines of " + typeRef_.getRawClass().getName();
-    linesRead = new Pair<String, String>(group, "Lines Read");
-    thriftStructsRead = new Pair<String, String>(group, "Thrift Structs");
-    thriftErrors = new Pair<String, String>(group, "Errors");
-
-    setLoaderSpec(getClass(), new String[]{thriftClassName});
-  }
-
-  public void skipToNextSyncPoint(boolean atFirstRecord) throws IOException {
-    // Since we are not block aligned we throw away the first record of each split and count on a different
-    // instance to read it.  The only split this doesn't work for is the first.
-    if (!atFirstRecord) {
-      getNext();
-    }
-  }
-
-  @Override
-  protected boolean verifyStream() throws IOException {
-    return is_ != null;
-  }
-
-  /**
-   * Return every non-null line as a single-element tuple to Pig.
-   */
-  public Tuple getNext() throws IOException {
-    if (!verifyStream()) {
-      return null;
-    }
-
-    String line;
-    Tuple t = null;
-    while ((line = is_.readLine(UTF8, RECORD_DELIMITER)) != null) {
-      incrCounter(linesRead, 1L);
-      M value = converter_.fromBytes(base64_.decode(line.getBytes("UTF-8")));
-      if (value != null) {
-        try {
-          t = thriftToPig_.getPigTuple(value);
-          incrCounter(thriftStructsRead, 1L);
-          break;
-        } catch (TException e) {
-          incrCounter(thriftErrors, 1L);
-          LOG.warn("ThriftToTuple error :", e); // may be struct mismatch
+    BinaryConverter<Tuple> tupleConverter = new BinaryConverter<Tuple>() {
+      public Tuple fromBytes(byte[] messageBuffer) {
+        M value = converter_.fromBytes(messageBuffer);
+        if (value != null) {
+          try {
+            return thriftToPig_.getPigTuple(value);
+          } catch (TException e) {
+            LOG.warn("ThriftToTuple error :", e); // may be struct mismatch
+          }
         }
+        return null;
       }
-    }
 
-    return t;
+      public byte[] toBytes(Tuple message) {
+        throw new RuntimeException("not implemented");
+      }
+    };
+
+    init(thriftClassName, tupleConverter);
+    setLoaderSpec(getClass(), new String[]{thriftClassName});
   }
 
   @Override
