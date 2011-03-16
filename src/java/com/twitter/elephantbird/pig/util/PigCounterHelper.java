3244c0ccb9c96cfbcba2d989da43999ff18ffd0f
Raghu Angadi <raghu@twitter.com>
2011-03-16 13:21:05 -0700

    PigUtilHelper : storing the reporter in singleton does not work when JVM is reused. The reporter goes stale as it points a already completed task.

--- src/java/com/twitter/elephantbird/pig/util/PigCounterHelper.java
+++ src/java/com/twitter/elephantbird/pig/util/PigCounterHelper.java
@@ -6,8 +6,6 @@ import com.google.common.collect.Maps;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigHadoopLogger;
 import org.apache.pig.impl.util.Pair;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * A helper class to deal with Hadoop counters in Pig.  They are stored within the singleton
@@ -18,7 +16,7 @@ import org.slf4j.LoggerFactory;
 public class PigCounterHelper {
   private Map<Pair<String, String>, Long> counterStringMap_ = Maps.newHashMap();
   private Map<Enum<?>, Long> counterEnumMap_ = Maps.newHashMap();
-  private Reporter reporter_ = null;
+  private PigHadoopLogger pigLogger_ = null;
 
   /**
    * Mocks the Reporter.incrCounter, but adds buffering.
@@ -64,9 +62,9 @@ public class PigCounterHelper {
    * @return the job's reporter object, or null if it isn't retrievable yet.
    */
   private Reporter getReporter() {
-    if (reporter_ == null) {
-      reporter_ = PigHadoopLogger.getInstance().getReporter();
+    if (pigLogger_ == null) {
+      pigLogger_ = PigHadoopLogger.getInstance();
     }
-    return reporter_;
+    return pigLogger_.getReporter();
   }
 }
