74fcdae038cc95d86f2b46415ded703c0c28f1b6
Angus He <angushe@gmail.com>
2011-03-29 13:56:36 +0800

    compile errors fix

--- src/java/com/twitter/elephantbird/pig/store/LzoThriftB64LinePigStorage.java
+++ src/java/com/twitter/elephantbird/pig/store/LzoThriftB64LinePigStorage.java
@@ -2,13 +2,13 @@ package com.twitter.elephantbird.pig.store;
 
 import java.io.IOException;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.pig.data.Tuple;
 import org.apache.thrift.TBase;
 
 import com.twitter.elephantbird.mapreduce.io.ThriftConverter;
 import com.twitter.elephantbird.pig.util.PigToThrift;
 import com.twitter.elephantbird.pig.util.PigUtil;
+import com.twitter.elephantbird.util.Base64;
 import com.twitter.elephantbird.util.Protobufs;
 import com.twitter.elephantbird.util.TypeRef;
 
@@ -22,7 +22,6 @@ import com.twitter.elephantbird.util.TypeRef;
 public class LzoThriftB64LinePigStorage<T extends TBase<?, ?>> extends LzoBaseStoreFunc {
 
   private TypeRef<T> typeRef;
-  private Base64 base64 = new Base64();
   private PigToThrift<T> pigToThrift;
   private ThriftConverter<T> converter;
 
@@ -32,10 +31,11 @@ public class LzoThriftB64LinePigStorage<T extends TBase<?, ?>> extends LzoBaseSt
     converter = ThriftConverter.newInstance(typeRef);
   }
 
-  public void putNext(Tuple f) throws IOException {
+  @Override
+public void putNext(Tuple f) throws IOException {
     if (f == null) return;
     T tObj = pigToThrift.getThriftObject(f);
-    os_.write(base64.encode(converter.toBytes(tObj)));
+    os_.write(Base64.encodeBytesToBytes(converter.toBytes(tObj)));
     os_.write(Protobufs.NEWLINE_UTF8_BYTE);
   }
 }
