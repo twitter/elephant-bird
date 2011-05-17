e0a06dc169928fd61a31377b8bb7a8d5c9b01348
Yifan Shi <sigmoids@TW-MBP13-YShi.local>
2011-05-17 15:45:28 -0700

    Add thrift input/output support for lzo compressed b64 line format, which is needed by deprecated by necessary hadoop setup.

---
+++ src/java/com/twitter/elephantbird/mapred/output/DeprecatedLzoOutputFormat.java
@@ -0,0 +1,35 @@
+package com.twitter.elephantbird.mapred.output;
+
+import com.hadoop.compression.lzo.LzopCodec;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobConf;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * Base class for Lzo outputformats.
+ *
+ * @author Yifan Shi
+ */
+public abstract class DeprecatedLzoOutputFormat <M, W>
+    extends FileOutputFormat<NullWritable, W> {
+
+  /**
+   * Helper method to create lzo output file needed to create RecordWriter
+   */
+  protected DataOutputStream getOutputStream(JobConf job)
+                  throws IOException {
+    LzopCodec codec = new LzopCodec();
+    codec.setConf(job);
+
+    Path file = getTaskOutputPath(job, codec.getDefaultExtension());
+    FileSystem fs = file.getFileSystem(job);
+    FSDataOutputStream fileOut = fs.create(file, false);
+    return new DataOutputStream(codec.createOutputStream(fileOut));
+  }
+}
