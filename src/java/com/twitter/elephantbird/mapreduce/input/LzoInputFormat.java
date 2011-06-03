d445c17ce36c492c516a84aeaa7a37592de68f61
dvryaboy <dvryaboy@gmail.com>
2011-06-02 20:21:44 -0700

    Make LzoInputFormat recursively examine subdirectories.

--- src/java/com/twitter/elephantbird/mapreduce/input/LzoInputFormat.java
+++ src/java/com/twitter/elephantbird/mapreduce/input/LzoInputFormat.java
@@ -7,11 +7,10 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import com.hadoop.compression.lzo.LzoIndex;
-import com.hadoop.compression.lzo.LzopCodec;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -19,41 +18,83 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Lists;
+import com.hadoop.compression.lzo.LzoIndex;
+
 /**
  * An {@link org.apache.hadoop.mapreduce.InputFormat} for lzop compressed files. This class
  * handles the nudging the input splits onto LZO boundaries using the existing LZO index files.
- * Subclass and implement getRecordReader to define custom LZO-based input formats.
+ * Subclass and implement getRecordReader to define custom LZO-based input formats.<p>
+ * <b>Note:</b> unlike the stock FileInputFormat, this recursively examines directories for matching files.
  */
 public abstract class LzoInputFormat<K, V> extends FileInputFormat<K, V> {
   private static final Logger LOG = LoggerFactory.getLogger(LzoInputFormat.class);
 
   private final Map<Path, LzoIndex> indexes_ = new HashMap<Path, LzoIndex>();
 
+  private final PathFilter visibleLzoFilter = new PathFilter() {
+
+    @Override
+    public boolean accept(Path path) {
+      String name = path.getName();
+      return !name.startsWith(".") &&
+      !name.startsWith("_") &&
+      name.endsWith(".lzo");
+    }};
+
+
   @Override
   protected List<FileStatus> listStatus(JobContext job) throws IOException {
     // The list of files is no different.
     List<FileStatus> files = super.listStatus(job);
-
-    // To help split the files at LZO boundaries, walk the list of lzo files and, if they
-    // have an associated index file, save that for later.
-    String fileExtension = new LzopCodec().getDefaultExtension();
+    List<FileStatus> results = Lists.newArrayList();
 
     Iterator<FileStatus> it = files.iterator();
     while (it.hasNext()) {
       FileStatus fileStatus = it.next();
       Path file = fileStatus.getPath();
-
-      if (!file.toString().endsWith(fileExtension)) {
-        // Get rid of non lzo files.
-        it.remove();
+      FileSystem fs = file.getFileSystem(job.getConfiguration());
+      if (fileStatus.isDir()) {
+        addInputPathRecursively(results, fs, file, visibleLzoFilter);
       } else {
-        // Read the index file.
-        LzoIndex index = LzoIndex.readIndex(file.getFileSystem(job.getConfiguration()), file);
-        indexes_.put(file, index);
+        if (visibleLzoFilter.accept(file)) {
+          results.add(fileStatus);
+        }
       }
     }
 
-    return files;
+    // To help split the files at LZO boundaries, walk the list of lzo files and, if they
+    // have an associated index file, save that for later.
+    for (FileStatus result : results) {
+      LzoIndex index = LzoIndex.readIndex(result.getPath().getFileSystem(job.getConfiguration()), result.getPath());
+      indexes_.put(result.getPath(), index);
+    }
+
+    return results;
+  }
+
+  //MAPREDUCE-1501
+  /**
+   * Add files in the input path recursively into the results.
+   * @param result
+   *          The List to store all files.
+   * @param fs
+   *          The FileSystem.
+   * @param path
+   *          The input path.
+   * @param inputFilter
+   *          The input filter that can be used to filter files/dirs.
+   * @throws IOException
+   */
+  protected void addInputPathRecursively(List<FileStatus> result,
+      FileSystem fs, Path path, PathFilter inputFilter) throws IOException {
+    for(FileStatus stat: fs.listStatus(path, inputFilter)) {
+      if (stat.isDir()) {
+        addInputPathRecursively(result, fs, stat.getPath(), inputFilter);
+      } else {
+        result.add(stat);
+      }
+    }
   }
 
   @Override
