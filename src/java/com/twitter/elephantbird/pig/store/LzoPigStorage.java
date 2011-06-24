4e30535bcf3072dc38c257e64b938e1d887fb0ee
Raghu Angadi <raghu@twitter.com>
2011-06-24 11:13:32 -0700

    Replace LzoTokenizedStorage with LzoPigStorage.

--- src/java/com/twitter/elephantbird/pig/store/LzoPigStorage.java
+++ src/java/com/twitter/elephantbird/pig/store/LzoPigStorage.java
@@ -11,8 +11,8 @@ import com.hadoop.compression.lzo.LzopCodec;
 import com.twitter.elephantbird.mapreduce.input.LzoTextInputFormat;
 
 /**
- * A wrapper for {@link PigStorage} that forces LZO compression for
- * storing. LzoTextInputFormat is used for loading since PigStorage
+ * A wrapper for {@link PigStorage} to enable LZO compression.
+ * LzoTextInputFormat is used for loading since PigStorage
  * can not split lzo files.
  *
  * This is similar to:
@@ -43,7 +43,7 @@ public class LzoPigStorage extends PigStorage {
     // looks like each context gets different job. modifying conf here
     // does not affect subsequent store statements in the script.
     job.getConfiguration().set("output.compression.enabled", "true");
-    job.getConfiguration().set("output.compression.code", LzopCodec.class.getName());
+    job.getConfiguration().set("output.compression.codec", LzopCodec.class.getName());
     super.setStoreLocation(location, job);
   }
 }
