eb8467d1a43ef268b01a7af3a5390b9c61c80c83
Raghu Angadi <raghu@twitter.com>
2011-07-26 14:57:44 -0700

    pull# 64: LzoOutputFormat indexing. tolerate older versions of hadoop-lzo.

--- src/java/com/twitter/elephantbird/mapreduce/output/LzoOutputFormat.java
+++ src/java/com/twitter/elephantbird/mapreduce/output/LzoOutputFormat.java
@@ -2,7 +2,9 @@ package com.twitter.elephantbird.mapreduce.output;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
+import com.hadoop.compression.lzo.LzoIndex;
 import com.hadoop.compression.lzo.LzopCodec;
 
 import org.apache.hadoop.conf.Configuration;
@@ -11,12 +13,33 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 /**
  * Base class for Lzo outputformats.
  * provides an helper method to create lzo output stream.
  */
 public abstract class LzoOutputFormat<K, V> extends FileOutputFormat<K, V> {
+
+  public static final Logger LOG = LogManager.getLogger(LzoOutputFormat.class);
+
+  /**
+   * A work-around to support environments with older versions of LzopCodec.
+   * It might not be feasible for to select right version of hadoop-lzo
+   * in some cases. This should be removed latest by EB-3.0.
+   */
+  private static boolean isLzopIndexSupported = false;
+  static {
+    try {
+      isLzopIndexSupported =
+        null != LzopCodec.class.getMethod("createIndexedOutputStream",
+                                          OutputStream.class,
+                                          DataOutputStream.class);
+    } catch (Exception e) {
+      // older version of hadoop-lzo.
+    }
+  }
 
   /**
    * Helper method to create lzo output file needed to create RecordWriter
@@ -31,16 +54,22 @@ public abstract class LzoOutputFormat<K, V> extends FileOutputFormat<K, V> {
     FileSystem fs = file.getFileSystem(conf);
     FSDataOutputStream fileOut = fs.create(file, false);
 
-    /* uncomment after https://github.com/kevinweil/hadoop-lzo/pull/23
     FSDataOutputStream indexOut = null;
     if (conf.getBoolean("elephantbird.lzo.output.index", false)) {
-      Path indexPath = new Path(file.getParent(), file.getName() + ".index");
-      indexOut = fs.create(indexPath, false);
+      if ( isLzopIndexSupported ) {
+        Path indexPath = file.suffix(LzoIndex.LZO_INDEX_SUFFIX);
+        indexOut = fs.create(indexPath, false);
+      } else {
+        LOG.warn("elephantbird.lzo.output.index is enabled, but LzopCodec "
+            + "does not have createIndexedOutputStream method. "
+            + "Please upgrade hadoop-lzo.");
+      }
     }
 
-    return new DataOutputStream(codec.createIndexedOutputStream(fileOut, indexOut));
-    */
+    OutputStream out = indexOut == null?
+        codec.createOutputStream(fileOut) :
+        codec.createIndexedOutputStream(fileOut, indexOut);
 
-    return new DataOutputStream(codec.createOutputStream(fileOut));
+    return new DataOutputStream(out);
   }
 }
