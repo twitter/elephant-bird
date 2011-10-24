1555de8e53cca873dd0599e06b70a07b5ec75630
Amund Tveit <amund@atbrox.com>
2011-10-24 15:46:42 +0200

    removed too aggressive stringifying

--- src/java/com/twitter/elephantbird/pig/store/JsonStorage.java
+++ src/java/com/twitter/elephantbird/pig/store/JsonStorage.java
@@ -3,13 +3,17 @@ package com.twitter.elephantbird.pig.store;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
+import java.util.List;
 
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.OutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
 import org.apache.pig.data.Tuple;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,8 +21,8 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.Sets;
 
 /**
- * A storage class to store the uncompressed output of each tuple as
- * json in a delimited file like PigStorage and LzoJsonStorage.
+ * A storage class to store the ouput of each tuple in a delimited file
+ * like PigStorage, but LZO compressed.
  */
 public class JsonStorage extends BaseStoreFunc {
     private static final Logger LOG = LoggerFactory.getLogger(JsonStorage.class);
@@ -37,6 +41,7 @@ public class JsonStorage extends BaseStoreFunc {
     LOG.info("Initialized JsonStorage. Keeping keys " + Joiner.on(", ").join(keysToKeep_) + ".");
   }
 
+
   /**
    * The first element is expected to be a map, or null. Anything else causes an error.
    * @param tuple the tuple to write.
@@ -47,23 +52,30 @@ public class JsonStorage extends BaseStoreFunc {
     json.clear();
     if (tuple != null && tuple.size() >= 1) {
       Map<String, Object> map = (Map<String, Object>) tuple.get(0);
-      if (keysToKeep_ == null) {
-        json.putAll(map);
-      } else {
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
-          if (keysToKeep_.contains(entry.getKey())) {
-            json.put(entry.getKey(), entry.getValue());
-          }
-        }
+      
+      if(map != null) {
+	  System.err.println("YOSAN: map = " + map);
+	  for (Map.Entry<String, Object> entry : map.entrySet()) {
+	      System.err.println("  YOSAN: key, value = " + entry.getKey() + "," + entry.getValue());
+	      if(entry.getValue().toString().startsWith("[")) {
+		  //unpack and pack :)
+		  JSONArray val = (JSONArray) JSONValue.parse(entry.getValue().toString());
+		  //List<String> val = (List<String>) entry.getValue();
+		  json.put(entry.getKey(), val);
+	      } else {
+		  json.put(entry.getKey(), entry.getValue());
+	      }
+	  }
+	  try {
+	      writer.write(null, new Text(json.toString()));
+	  } catch (InterruptedException e) {
+	      // Under what circumstances does this happen?
+	      throw new IOException(e);
+	  }
       }
     }
-    try {
-      writer.write(null, new Text(json.toString()));
-    } catch (InterruptedException e) {
-      // Under what circumstances does this happen?
-      throw new IOException(e);
-    }
   }
+
 
   @Override
   public OutputFormat<NullWritable, Text> getOutputFormat() {
