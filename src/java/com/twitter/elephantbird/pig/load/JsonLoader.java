d0366f5ebd67c65caca3e2ab84728722319c5883
Travis Crawford <travis@twitter.com>
2012-02-22 18:36:43 -0800

    Refactor JSON loaders so an InputFormat can be used & update test. A future change should remove the now deprecated LzoJsonLoader (as it contains nothing LZO related except a hard-coded InputFormat).

--- src/java/com/twitter/elephantbird/pig/load/JsonLoader.java
+++ src/java/com/twitter/elephantbird/pig/load/JsonLoader.java
@@ -1,16 +1,17 @@
 package com.twitter.elephantbird.pig.load;
 
-import java.io.IOException;
-import java.util.Map;
-
+import com.google.common.collect.Maps;
+import com.twitter.elephantbird.pig.util.PigCounterHelper;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.pig.LoadFunc;
 import org.apache.pig.PigException;
 import org.apache.pig.backend.executionengine.ExecException;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigSplit;
-import org.apache.pig.builtin.PigStorage;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
 import org.json.simple.JSONObject;
@@ -19,43 +20,81 @@ import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Maps;
-import com.twitter.elephantbird.pig.load.LzoJsonLoader.LzoJsonLoaderCounters;
-import com.twitter.elephantbird.pig.util.PigCounterHelper;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
 
 /**
- * A basic Json Loader. Totally subject to change, this is mostly a cut and paste job.
+ * Decodes each line as JSON passes the resulting map of values
+ * to Pig as a single-element tuple.
  */
-public class JsonLoader extends PigStorage {
-
+public class JsonLoader extends LoadFunc {
   private static final Logger LOG = LoggerFactory.getLogger(JsonLoader.class);
-  @SuppressWarnings("rawtypes")
-  protected RecordReader reader_;
+  private static final TupleFactory tupleFactory = TupleFactory.getInstance();
 
-  private static final TupleFactory tupleFactory_ = TupleFactory.getInstance();
+  private final JSONParser jsonParser = new JSONParser();
 
-  private final JSONParser jsonParser_ = new JSONParser();
+  private final PigCounterHelper counterHelper = new PigCounterHelper();
 
-  protected enum JsonLoaderCounters { LinesRead, LinesJsonDecoded, LinesParseError, LinesParseErrorBadNumber }
+  private enum JsonLoaderCounters {
+    LinesRead,
+    LinesJsonDecoded,
+    LinesParseError,
+    LinesParseErrorBadNumber
+  }
 
-  // Making accessing Hadoop counters from Pig slightly more convenient.
-  private final PigCounterHelper counterHelper_ = new PigCounterHelper();
+  private RecordReader reader;
+  private String inputFormatClassName;
+  private FileInputFormat inputFormat;
+
+  public JsonLoader() {
+    this(TextInputFormat.class.getName());
+  }
+
+  public JsonLoader(String inputFormatClassName) {
+    this.inputFormatClassName = inputFormatClassName;
+    try {
+      this.inputFormat = (FileInputFormat) Class.forName(inputFormatClassName).newInstance();
+    } catch (InstantiationException e) {
+      throw new RuntimeException("Failed creating input format " + inputFormatClassName, e);
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException("Failed creating input format " + inputFormatClassName, e);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException("Failed creating input format " + inputFormatClassName, e);
+    }
+  }
+
+  @Override
+  public void setLocation(String location, Job job) throws IOException {
+    try {
+      Method m = inputFormat.getClass().getMethod("setInputPaths",
+          new Class[]{Job.class, String.class});
+      m.invoke(null, job, location);
+    } catch (NoSuchMethodException e) {
+      throw new RuntimeException("Failed setting input paths on " + inputFormatClassName, e);
+    } catch (InvocationTargetException e) {
+      throw new RuntimeException("Failed setting input paths on " + inputFormatClassName, e);
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException("Failed setting input paths on " + inputFormatClassName, e);
+    }
+  }
 
   /**
    * Return every non-null line as a single-element tuple to Pig.
    */
   @Override
   public Tuple getNext() throws IOException {
-    if (reader_ == null) {
+    if (reader == null) {
       return null;
     }
     try {
-      while (reader_.nextKeyValue()) {
-        Text value_ = (Text)reader_.getCurrentValue();
-        incrCounter(LzoJsonLoaderCounters.LinesRead, 1L);
-        Tuple t = parseStringToTuple(value_.toString());
+      while (reader.nextKeyValue()) {
+        Text value = (Text) reader.getCurrentValue();
+        counterHelper.incrCounter(JsonLoaderCounters.LinesRead, 1L);
+        Tuple t = parseStringToTuple(value.toString());
         if (t != null) {
-          incrCounter(LzoJsonLoaderCounters.LinesJsonDecoded, 1L);
+          counterHelper.incrCounter(JsonLoaderCounters.LinesJsonDecoded, 1L);
           return t;
         }
       }
@@ -70,57 +109,44 @@ public class JsonLoader extends PigStorage {
   }
 
   @Override
-  public void prepareToRead(@SuppressWarnings("rawtypes") RecordReader reader, PigSplit split) {
-      this.reader_ = reader;
+  public void prepareToRead(RecordReader reader, PigSplit split) {
+    this.reader = reader;
   }
 
-  // We could implement JsonInputFormat and proxy to it, and maybe that'd be worthwhile,
-  // but taking the cheap shortcut for now.
-  @SuppressWarnings("rawtypes")
   @Override
   public InputFormat getInputFormat() {
-      return new TextInputFormat();
-  }
-
-  /**
-   * A convenience function for working with Hadoop counter objects from load functions.  The Hadoop
-   * reporter object isn't always set up at first, so this class provides brief buffering to ensure
-   * that counters are always recorded.
-   */
-  protected void incrCounter(Enum<?> key, long incr) {
-    counterHelper_.incrCounter(key, incr);
+    return inputFormat;
   }
 
   protected Tuple parseStringToTuple(String line) {
     try {
       Map<String, String> values = Maps.newHashMap();
-      JSONObject jsonObj = (JSONObject)jsonParser_.parse(line);
+      JSONObject jsonObj = (JSONObject) jsonParser.parse(line);
       if (jsonObj != null) {
-        for (Object key: jsonObj.keySet()) {
+        for (Object key : jsonObj.keySet()) {
           Object value = jsonObj.get(key);
           values.put(key.toString(), value != null ? value.toString() : null);
         }
+      } else {
+        // JSONParser#parse(String) may return a null reference, e.g. when
+        // the input parameter is the string "null".  A single line with
+        // "null" is not valid JSON though.
+        LOG.warn("Could not json-decode string: " + line);
+        counterHelper.incrCounter(JsonLoaderCounters.LinesParseError, 1L);
+        return null;
       }
-      else {
-          // JSONParser#parse(String) may return a null reference, e.g. when
-          // the input parameter is the string "null".  A single line with
-          // "null" is not valid JSON though.
-          LOG.warn("Could not json-decode string: " + line);
-          incrCounter(JsonLoaderCounters.LinesParseError, 1L);
-          return null;
-      }
-      return tupleFactory_.newTuple(values);
+      return tupleFactory.newTuple(values);
     } catch (ParseException e) {
       LOG.warn("Could not json-decode string: " + line, e);
-      incrCounter(JsonLoaderCounters.LinesParseError, 1L);
+      counterHelper.incrCounter(JsonLoaderCounters.LinesParseError, 1L);
       return null;
     } catch (NumberFormatException e) {
       LOG.warn("Very big number exceeds the scale of long: " + line, e);
-      incrCounter(JsonLoaderCounters.LinesParseErrorBadNumber, 1L);
+      counterHelper.incrCounter(JsonLoaderCounters.LinesParseErrorBadNumber, 1L);
       return null;
     } catch (ClassCastException e) {
       LOG.warn("Could not convert to Json Object: " + line, e);
-      incrCounter(JsonLoaderCounters.LinesParseError, 1L);
+      counterHelper.incrCounter(JsonLoaderCounters.LinesParseError, 1L);
       return null;
     }
   }
