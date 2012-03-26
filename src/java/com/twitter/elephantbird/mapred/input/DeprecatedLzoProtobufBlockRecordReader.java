12271352ce87bc5f192a23943f6c8e1d7ba55128
Argyris Zymnis <argyris@twitter.com>
2012-03-26 15:03:42 -0700

    Remove unused imports.

--- src/java/com/twitter/elephantbird/mapred/input/DeprecatedLzoProtobufBlockRecordReader.java
+++ src/java/com/twitter/elephantbird/mapred/input/DeprecatedLzoProtobufBlockRecordReader.java
@@ -3,20 +3,8 @@ package com.twitter.elephantbird.mapred.input;
 import com.google.protobuf.Message;
 import com.twitter.elephantbird.mapreduce.io.BinaryBlockReader;
 import com.twitter.elephantbird.mapreduce.io.ProtobufBlockReader;
-import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
-import com.twitter.elephantbird.util.TypeRef;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.compress.CompressionCodec;
-import org.apache.hadoop.io.compress.CompressionCodecFactory;
-import org.apache.hadoop.mapred.FileSplit;
-import org.apache.hadoop.mapred.RecordReader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,7 +17,6 @@ import java.io.InputStream;
 @SuppressWarnings("deprecation")
 public class DeprecatedLzoProtobufBlockRecordReader<M extends Message>
     extends DeprecatedLzoBlockRecordReader<M> {
-
   protected BinaryBlockReader<M> createInputReader(InputStream input, Configuration conf) throws IOException {
     return new ProtobufBlockReader<M>(input, typeRef_);
   }
