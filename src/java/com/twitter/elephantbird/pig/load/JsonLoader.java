88049cf9000f064a476f275adbd0f3a430d59388
André Panisson <panisson@gmail.com>
2012-03-31 01:28:23 +0200

    Load nested json structures: objects and arrays. Objects are loaded as nested Maps. Arrays are loaded as Bags.

--- src/java/com/twitter/elephantbird/pig/load/JsonLoader.java
+++ src/java/com/twitter/elephantbird/pig/load/JsonLoader.java
@@ -7,9 +7,13 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.pig.PigException;
 import org.apache.pig.backend.executionengine.ExecException;
+import org.apache.pig.data.BagFactory;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.DefaultBagFactory;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
 import org.apache.pig.impl.PigContext;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -26,6 +30,7 @@ import java.util.Map;
 public class JsonLoader extends LzoBaseLoadFunc {
   private static final Logger LOG = LoggerFactory.getLogger(JsonLoader.class);
   private static final TupleFactory tupleFactory = TupleFactory.getInstance();
+  private static final BagFactory bagFactory = DefaultBagFactory.getInstance();
 
   private final JSONParser jsonParser = new JSONParser();
 
@@ -87,13 +92,9 @@ public class JsonLoader extends LzoBaseLoadFunc {
 
   protected Tuple parseStringToTuple(String line) {
     try {
-      Map<String, String> values = Maps.newHashMap();
       JSONObject jsonObj = (JSONObject) jsonParser.parse(line);
       if (jsonObj != null) {
-        for (Object key : jsonObj.keySet()) {
-          Object value = jsonObj.get(key);
-          values.put(key.toString(), value != null ? value.toString() : null);
-        }
+        return tupleFactory.newTuple(walkJson(jsonObj));
       } else {
         // JSONParser#parse(String) may return a null reference, e.g. when
         // the input parameter is the string "null".  A single line with
@@ -102,7 +103,6 @@ public class JsonLoader extends LzoBaseLoadFunc {
         incrCounter(JsonLoaderCounters.LinesParseError, 1L);
         return null;
       }
-      return tupleFactory.newTuple(values);
     } catch (ParseException e) {
       LOG.warn("Could not json-decode string: " + line, e);
       incrCounter(JsonLoaderCounters.LinesParseError, 1L);
@@ -118,4 +118,31 @@ public class JsonLoader extends LzoBaseLoadFunc {
     }
   }
 
+  private Object wrap(Object value) {
+    
+    if (value instanceof JSONObject) {
+      return walkJson((JSONObject) value);
+    }  else if (value instanceof JSONArray) {
+      
+      JSONArray a = (JSONArray) value;
+      DataBag mapValue = bagFactory.newDefaultBag();
+      for (int i=0; i<a.size(); i++) {
+        Tuple t = tupleFactory.newTuple(wrap(a.get(i)));
+        mapValue.add(t);
+      }
+      return mapValue;
+      
+    } else {
+      return value != null ? value.toString() : null;
+    }
+  }
+
+  private Map<String,Object> walkJson(JSONObject jsonObj) {
+    Map<String,Object> v = Maps.newHashMap();
+    for (Object key: jsonObj.keySet()) {
+      v.put(key.toString(), wrap(jsonObj.get(key)));
+    }
+    return v;
+  }
+  
 }
