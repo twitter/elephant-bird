7196ab3721311d628a2f933f37e2aa7c15679ead
Travis Crawford <travis@twitter.com>
2012-04-12 18:09:38 -0700

    Add HCatalog support to HiveMultiInputFormat. As HCatalog and Hive store their metadata differently in the job conf we need to support both use cases.

--- src/java/com/twitter/elephantbird/mapred/input/HiveMultiInputFormat.java
+++ src/java/com/twitter/elephantbird/mapred/input/HiveMultiInputFormat.java
@@ -3,6 +3,7 @@ package com.twitter.elephantbird.mapred.input;
 import com.twitter.elephantbird.mapreduce.input.MultiInputFormat;
 import com.twitter.elephantbird.mapreduce.io.BinaryWritable;
 import com.twitter.elephantbird.util.TypeRef;
+import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.exec.Utilities;
 import org.apache.hadoop.hive.ql.plan.PartitionDesc;
 import org.apache.hadoop.hive.serde.Constants;
@@ -12,6 +13,11 @@ import org.apache.hadoop.mapred.InputSplit;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.RecordReader;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.hcatalog.common.HCatConstants;
+import org.apache.hcatalog.common.HCatUtil;
+import org.apache.hcatalog.mapreduce.InputJobInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Map;
@@ -26,23 +32,40 @@ import java.util.Properties;
 public class HiveMultiInputFormat
     extends DeprecatedInputFormatWrapper<LongWritable, BinaryWritable> {
 
+  private static final Logger LOG = LoggerFactory.getLogger(HiveMultiInputFormat.class);
+
   public HiveMultiInputFormat() {
     super(new MultiInputFormat());
   }
 
-  private void initialize(FileSplit split, JobConf job) {
-    Map<String, PartitionDesc> partitionDescMap =
-        Utilities.getMapRedWork(job).getPathToPartitionInfo();
+  private void initialize(FileSplit split, JobConf job) throws IOException {
+    LOG.info("Initializing HiveMultiInputFormat for " + split + " with job " + job);
 
-    if (!partitionDescMap.containsKey(split.getPath().getParent().toUri().toString())) {
-      throw new RuntimeException("Failed locating partition description for "
-          + split.getPath().toUri().toString());
+    String thriftClassName = null;
+    Properties properties = null;
+
+    if (!"".equals(HiveConf.getVar(job, HiveConf.ConfVars.PLAN))) {
+      // Running as a Hive query. Use MapredWork for metadata.
+      Map<String, PartitionDesc> partitionDescMap =
+          Utilities.getMapRedWork(job).getPathToPartitionInfo();
+
+      if (!partitionDescMap.containsKey(split.getPath().getParent().toUri().toString())) {
+        throw new RuntimeException("Failed locating partition description for "
+            + split.getPath().toUri().toString());
+      }
+      properties = partitionDescMap.get(split.getPath().getParent().toUri().toString())
+          .getTableDesc().getProperties();
+    } else if (job.get(HCatConstants.HCAT_KEY_JOB_INFO, null) != null) {
+      // Running as an HCatalog query. Use InputJobInfo for metadata.
+      InputJobInfo inputJobInfo = (InputJobInfo) HCatUtil.deserialize(
+          job.get(HCatConstants.HCAT_KEY_JOB_INFO));
+      properties = inputJobInfo.getTableInfo().getStorerInfo().getProperties();
     }
 
-    Properties properties = partitionDescMap.get(split.getPath().getParent().toUri().toString())
-        .getTableDesc().getProperties();
+    if (properties != null) {
+      thriftClassName = properties.getProperty(Constants.SERIALIZATION_CLASS);
+    }
 
-    String thriftClassName = properties.getProperty(Constants.SERIALIZATION_CLASS, null);
     if (thriftClassName == null) {
       throw new RuntimeException(
           "Required property " + Constants.SERIALIZATION_CLASS + " is null.");
