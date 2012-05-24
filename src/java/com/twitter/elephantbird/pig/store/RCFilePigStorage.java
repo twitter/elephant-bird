c46b0ae27d8871d9566aa62b6959491824b48668
Raghu Angadi <raghu@twitter.com>
2012-05-24 01:47:05 -0700

    Load support for RCFilePigStorage

--- src/java/com/twitter/elephantbird/pig/store/RCFilePigStorage.java
+++ src/java/com/twitter/elephantbird/pig/store/RCFilePigStorage.java
@@ -1,14 +1,16 @@
 package com.twitter.elephantbird.pig.store;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Properties;
 
+import org.apache.hadoop.hive.ql.io.RCFileInputFormat;
 import org.apache.hadoop.hive.serde2.ByteStream;
+import org.apache.hadoop.hive.serde2.ColumnProjectionUtils;
 import org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable;
 import org.apache.hadoop.hive.serde2.columnar.BytesRefWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.Job;
@@ -17,37 +19,51 @@ import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.pig.ResourceSchema;
 import org.apache.pig.builtin.PigStorage;
+import org.apache.pig.data.DataByteArray;
 import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.apache.pig.impl.logicalLayer.FrontendException;
+import org.apache.pig.impl.util.ObjectSerializer;
 import org.apache.pig.impl.util.StorageUtil;
 import org.apache.pig.impl.util.UDFContext;
 
+import com.twitter.elephantbird.mapreduce.input.MapReduceInputFormatWrapper;
 import com.twitter.elephantbird.mapreduce.output.RCFileOutputFormat;
 
 /**
- * TODO:
- * RCFile version of PigStorage. Stores each top level field as a text field.
- * Loading is not supported yet.
+ * RCFile version of PigStorage.
  *
  * Usage: <pre>
  * register 'libs/*hive-common*.jar; -- hive-common for RCFile
  * register 'libs/*hive-exec*.jar;  -- hive-exec for RCFile
  * register 'libs/*protobuf-java*.jar; -- protobuf-java should not be required, but for now it is.
  *
- * a = load 'x.txt' as (a, b);
- * store a into 'output' using RCFilePigStorage();
+ * a = load 'input' using RCFileStorage() as (a:int, b:chararray, c:long);
+ *
+ * b = foreach a generate a, TOTUPLE(a, c);
+ * store b into 'output' using RCFilePigStorage();
+ *
  * </pre>
  */
 public class RCFilePigStorage extends PigStorage {
 
+  private TupleFactory tupleFactory = TupleFactory.getInstance();
   private int numColumns = -1;
+  private int[] requiredColumns = null;
 
   public RCFilePigStorage() {
     super();
   }
 
+  private Properties getUDFProperties() {
+    return UDFContext.getUDFContext()
+              .getUDFProperties(this.getClass(), new String[] { signature });
+  }
+
   @Override
-  public InputFormat<LongWritable, Text> getInputFormat() {
-    throw new RuntimeException("LoadFunc is not supported yet. on TODO list.");
+  public InputFormat<LongWritable, BytesRefArrayWritable> getInputFormat() {
+    return new MapReduceInputFormatWrapper<LongWritable, BytesRefArrayWritable>
+            (new RCFileInputFormat<LongWritable, BytesRefArrayWritable>());
   }
 
   @Override
@@ -55,20 +71,44 @@ public class RCFilePigStorage extends PigStorage {
     return new TupleOutputFormat();
   }
 
+  public void setLocation(String location, Job job) throws IOException {
+    super.setLocation(location, job);
+
+    // sets columnIds config for RCFile
+
+    String obj = getUDFProperties().getProperty("requiredFieldList");
+    if (obj == null) {
+      // front end or there is no projection set
+      ColumnProjectionUtils.setFullyReadColumns(job.getConfiguration());
+      return ;
+    }
+
+    RequiredFieldList fieldList = (RequiredFieldList)
+                                  ObjectSerializer.deserialize(obj);
+
+    ArrayList<Integer> ids = new ArrayList<Integer>();
+    requiredColumns = new int[fieldList.getFields().size()];
+    int i = 0;
+    for (RequiredField rf : fieldList.getFields()) {
+      requiredColumns[i++] = rf.getIndex();
+      ids.add(rf.getIndex());
+    }
+
+    ColumnProjectionUtils.setReadColumnIDs(job.getConfiguration(), ids);
+  }
+
   @Override
   public void checkSchema(ResourceSchema s) throws IOException {
     super.checkSchema(s);
-    UDFContext.getUDFContext()
-      .getUDFProperties(this.getClass(), new String[]{signature})
-      .setProperty("numColumns", Integer.toString(s.getFields().length));
+    getUDFProperties().setProperty("numColumns",
+                                   Integer.toString(s.getFields().length));
   }
 
   @Override
   public void setStoreLocation(String location, Job job) throws IOException {
     super.setStoreLocation(location, job);
     // set number of columns if this is set in context.
-    Properties p = UDFContext.getUDFContext()
-                  .getUDFProperties(this.getClass(), new String[]{signature});
+    Properties p = getUDFProperties();
     if (p != null) {
       numColumns = Integer.parseInt(p.getProperty("numColumns", "-1"));
     }
@@ -78,7 +118,53 @@ public class RCFilePigStorage extends PigStorage {
     }
   }
 
+  @Override
+  public RequiredFieldResponse pushProjection(RequiredFieldList requiredFieldList)
+                                             throws FrontendException {
+    // don't need to invoke super.pushProjection();
+    try {
+      getUDFProperties().setProperty("requiredFieldList",
+                                     ObjectSerializer.serialize(requiredFieldList));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
 
+    return new RequiredFieldResponse(true);
+  }
+
+  @Override
+  public Tuple getNext() throws IOException {
+    try {
+      if (!in.nextKeyValue()) {
+        return null;
+      }
+
+      BytesRefArrayWritable byteRefs = (BytesRefArrayWritable) in.getCurrentValue();
+
+      boolean isProjected = requiredColumns != null;
+      int inputSize = byteRefs.size();
+      int tupleSize = isProjected ? requiredColumns.length : inputSize;
+
+      Tuple tuple = tupleFactory.newTuple(tupleSize);
+      int tupleIdx = 0;
+
+      for (int i=0; i<inputSize; i++) {
+        BytesRefWritable ref = byteRefs.get(i);
+        if (ref != null && (!isProjected || i == requiredColumns[tupleIdx])) {
+          tuple.set(tupleIdx++, new DataByteArray(ref.getBytesCopy()));
+        }
+      }
+
+      return tuple;
+    } catch (InterruptedException e) {
+      throw new IOException(e);
+    }
+  }
+
+  /**
+   * converts output tuple to set of byte arrays and writes them to the RCFile.
+   * It converts the tuple fields to bytes the same way as PigStorage does.
+   */
   private class TupleOutputFormat extends RCFileOutputFormat {
 
     @Override
@@ -128,7 +214,6 @@ public class RCFilePigStorage extends PigStorage {
 
           writer.write(null, rowWritable);
         }
-
       };
     }
   }
