4c476b9a189c5b60547d4268973beb99ab92e0f5
James Estes <james.estes@jivesoftware.com>
2012-06-07 14:04:58 -0600

    Changed JsonStringToMap to delegate to the JsonToPigParser.
    I wish I could figure out how to get nested pig maps to work from
    EvalFuncs.  It works from LoadFuncs.  Until then, the
    JsonStringToMap usage will be limited to flat maps, and multiple
    calls to continue nesting through structures, using the eval func
    on the inner json maps.

--- src/java/com/twitter/elephantbird/pig/piggybank/JsonStringToMap.java
+++ src/java/com/twitter/elephantbird/pig/piggybank/JsonStringToMap.java
@@ -1,78 +1,77 @@
 package com.twitter.elephantbird.pig.piggybank;
-
-import java.io.IOException;
-import java.util.Map;
-
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.apache.pig.EvalFunc;
 import org.apache.pig.backend.executionengine.ExecException;
 import org.apache.pig.data.Tuple;
-import org.apache.pig.impl.logicalLayer.schema.Schema;
-import org.apache.pig.impl.util.Utils;
-import org.apache.pig.parser.ParserException;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-import com.google.common.collect.Maps;
-import com.twitter.elephantbird.pig.util.PigCounterHelper;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Transforms a Json string into a Pig map whose value type is chararray. Only goes one level deep;
- * All input map values are converted to strings via {@link Object#toString()}.
+ * Delegates to {@link JsonToPigParser} with its defaults (don't create nested maps from nested json, and don't detect
+ * json in all string property values).
+ *
+ * Usage Example:
+ * Given the following json content:
+ * <pre>
+ {"a":1,"message":"{\"b\":2}",detail:{"b":2}}
+ {"a":3,"message":"{\"b\":3}",detail:{"b":2}}
+ {"a":2,"message":"{\"b\":1}",detail:{"b":3}}
+ {"a":9,"message":"{\"b\":22}",detail:{"b":7}}
+ {"a":15,"message":"{\"b\":25}",detail:{"b":9}}
+ {"a":7,"message":"{\"b\":52}",detail:{"b":11}}
+ </pre>
+ * The following pig script will parse out the "a" values from the top level:
+ * <pre>
+ json_strings = LOAD '/test/foo.json' as (json_string);
+ maps = FOREACH json_strings GENERATE com.jivesoftware.pig.JsonToStringMap((chararray)json_string) as json;
+ aVals = foreach maps generate json#'a') as aVal;
+ dump aVals
+ -- Output:
+ (1)
+ (3)
+ (2)
+ (9)
+ (15)
+ (7)
+ </pre>
+ * Getting the EvalFunc to generate nested maps proved to be an exercise in futility (it seems to work just fine from
+ * loaders though:  see {@link com.twitter.elephantbird.pig.util.JsonConverter}.  However, you can get at data under the detail key with:
+ <pre>
+ maps = FOREACH json_strings GENERATE com.jivesoftware.pig.JsonStringToMap((chararray)json_string) as json;
+ details = foreach maps generate com.jivesoftware.pig.JsonStringToMap((chararray)json#'detail') as detail;
+ dump details
+ -- Output:
+ ([b#2])
+ ([b#2])
+ ([b#3])
+ ([b#7])
+ ([b#9])
+ ([b#11])
+ </pre>
  */
-public class JsonStringToMap extends EvalFunc<Map<String, String>> {
-  private static final Logger LOG = LogManager.getLogger(JsonStringToMap.class);
-  private final JSONParser jsonParser = new JSONParser();
-  private final PigCounterHelper counterHelper = new PigCounterHelper();
+public class JsonStringToMap extends EvalFunc<Map> {
+    private final JsonToPigParser parser = JsonToPigParser.DEFAULT;
 
-  @Override
-  public Schema outputSchema(Schema input) {
-    try {
-      return Utils.getSchemaFromString("json: [chararray]");
-    } catch (ParserException e) {
-      throw new RuntimeException(e);
+    @Override
+    public Map exec(Tuple input) throws IOException {
+        try {
+            if (input == null || input.size() < 1) {
+                throw new IOException("Not enough arguments to " + this.getClass().getName() + ": expected at least 1");
+            }
+
+            String json = (String) input.get(0);
+            Map<String, Object> map = parser.toPigMap(json);
+            if(map == null || !parser.isNestedJson()){
+                return map;
+            }
+            Map<String, String> stringMap = new HashMap<String,String>();
+            for(Map.Entry<String,Object> entry : map.entrySet()){
+                stringMap.put(entry.getKey(), String.valueOf(entry.getValue()));
+            }
+            return stringMap;
+        } catch (ExecException e) {
+            throw new IOException(e);
+        }
     }
-  }
-
-  @Override
-  public Map<String, String> exec(Tuple input) throws IOException {
-    try {
-      // Verify the input is valid, logging to a Hadoop counter if not.
-      if (input == null || input.size() < 1) {
-        throw new IOException("Not enough arguments to " + this.getClass().getName() + ": got " + input.size() + ", expected at least 1");
-      }
-
-      if (input.get(0) == null) {
-        counterHelper.incrCounter(getClass().getName(), "NullJsonString", 1L);
-        return null;
-      }
-
-      String jsonLiteral = (String) input.get(0);
-      return parseStringToMap(jsonLiteral);
-    } catch (ExecException e) {
-      LOG.warn("Error in " + getClass() + " with input " + input, e);
-      throw new IOException(e);
-    }
-  }
-
-  protected Map<String, String> parseStringToMap(String line) {
-    try {
-      Map<String, String> values = Maps.newHashMap();
-      JSONObject jsonObj = (JSONObject) jsonParser.parse(line);
-      for (Object key : jsonObj.keySet()) {
-        Object value = jsonObj.get(key);
-        values.put(key.toString(), value != null ? value.toString() : null);
-      }
-      return values;
-    } catch (ParseException e) {
-      LOG.warn("Could not json-decode string: " + line, e);
-      return null;
-    } catch (NumberFormatException e) {
-      LOG.warn("Very big number exceeds the scale of long: " + line, e);
-      return null;
-    }
-  }
-
 }
