57a6f3007ead5ed9ff97a1b241179f569f56fecb
Alex Levenson <alexlevenson@twitter.com>
2012-12-06 16:29:21 -0800

    use built in hit count collector

--- lucene/src/main/java/com/twitter/elephantbird/mapreduce/input/LuceneIndexCountHitsRecordReader.java
+++ lucene/src/main/java/com/twitter/elephantbird/mapreduce/input/LuceneIndexCountHitsRecordReader.java
@@ -6,11 +6,10 @@ import java.util.List;
 import com.google.common.collect.ImmutableList;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.lucene.index.AtomicReaderContext;
-import org.apache.lucene.search.Collector;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.Scorer;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.TotalHitCountCollector;
 
 /**
  * Only counts the number of hits for each query
@@ -22,34 +21,8 @@ public abstract class LuceneIndexCountHitsRecordReader
 
   @Override
   protected List<IntWritable> search(IndexSearcher searcher, Query query) throws IOException {
-    CountHitsCollector collector = new CountHitsCollector();
+    TotalHitCountCollector collector = new TotalHitCountCollector();
     searcher.search(query, collector);
-    return ImmutableList.of(new IntWritable(collector.getHits()));
-  }
-
-  private static final class CountHitsCollector extends Collector {
-    private int hits = 0;
-
-    @Override
-    public void setScorer(Scorer scorer) throws IOException {
-    }
-
-    @Override
-    public void collect(int i) throws IOException {
-      hits++;
-    }
-
-    @Override
-    public void setNextReader(AtomicReaderContext atomicReaderContext) throws IOException {
-    }
-
-    @Override
-    public boolean acceptsDocsOutOfOrder() {
-      return true;
-    }
-
-    public int getHits() {
-      return hits;
-    }
+    return ImmutableList.of(new IntWritable(collector.getTotalHits()));
   }
 }
