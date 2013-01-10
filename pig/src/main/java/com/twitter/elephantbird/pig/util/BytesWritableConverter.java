eded5fd0748cfff27d5a8099e80eb06ee267e990
Andy Schlaikjer <hazen@twitter.com>
2013-01-10 09:17:59 -0800

    Adds leading 4 bytes run length test

--- pig/src/main/java/com/twitter/elephantbird/pig/util/BytesWritableConverter.java
+++ pig/src/main/java/com/twitter/elephantbird/pig/util/BytesWritableConverter.java
@@ -3,7 +3,7 @@ package com.twitter.elephantbird.pig.util;
 import java.io.IOException;
 
 import org.apache.hadoop.io.BytesWritable;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DataInputBuffer;
 import org.apache.pig.ResourceSchema.ResourceFieldSchema;
 import org.apache.pig.data.DataByteArray;
 import org.apache.pig.data.DataType;
@@ -14,6 +14,8 @@ import org.apache.pig.data.DataType;
  * @author Andy Schlaikjer
  */
 public class BytesWritableConverter extends AbstractWritableConverter<BytesWritable> {
+  private final DataInputBuffer in = new DataInputBuffer();
+
   public BytesWritableConverter() {
     super(new BytesWritable());
   }
@@ -28,7 +30,14 @@ public class BytesWritableConverter extends AbstractWritableConverter<BytesWrita
   @Override
   public Object bytesToObject(DataByteArray dataByteArray) throws IOException {
     byte[] bytes = dataByteArray.get();
-    // strip leading 4 bytes which encode run length of data
+    // test leading 4 bytes encode run length of rest of data
+    in.reset(bytes, bytes.length);
+    int length = in.readInt();
+    if (length != bytes.length - 4) {
+      throw new IOException(String.format(
+          "Int value '%d' of leading four bytes does not match run length of data '%d'",
+          length, bytes.length - 4));
+    }
     return new DataByteArray(bytes, 4, bytes.length);
   }
 
@@ -43,8 +52,7 @@ public class BytesWritableConverter extends AbstractWritableConverter<BytesWrita
 
   @Override
   protected BytesWritable toWritable(DataByteArray value) throws IOException {
-    byte[] bytes = value.get();
-    writable.set(bytes, 0, bytes.length);
-    return writable;
+    // avoid array copy at the sake of new BytesWritable
+    return new BytesWritable(value.get());
   }
 }
