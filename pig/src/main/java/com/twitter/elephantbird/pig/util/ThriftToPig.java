dbe29f63df950fd5cfca9f399300102a8a7ccc3e
Feng Peng <feng@twitter.com>
2013-01-25 14:59:19 -0800

    Remove unused import.

--- pig/src/main/java/com/twitter/elephantbird/pig/util/ThriftToPig.java
+++ pig/src/main/java/com/twitter/elephantbird/pig/util/ThriftToPig.java
@@ -4,7 +4,6 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
