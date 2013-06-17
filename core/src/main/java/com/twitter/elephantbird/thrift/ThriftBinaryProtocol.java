1bf90bee0b6f36c33119d6321beb5da1d7731353
Raghu Angadi <raghu@twitter.com>
2013-06-17 13:21:45 -0700

    Move TBinaryProtocol extension into its own class (and factory) Factory makes it more usable in different contexts.

---
+++ core/src/main/java/com/twitter/elephantbird/thrift/ThriftBinaryProtocol.java
@@ -0,0 +1,79 @@
+package com.twitter.elephantbird.thrift;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TList;
+import org.apache.thrift.protocol.TMap;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
+import org.apache.thrift.protocol.TSet;
+import org.apache.thrift.protocol.TType;
+import org.apache.thrift.transport.TTransport;
+
+/**
+ * Functionally exactly same as {@link TBinaryProtocol}. <p>
+ *
+ * Overwrites a few methods so that some malformed messages don't end up
+ * taking excessively large amounts of cpu inside TProtocolUtil.skip().
+ */
+public class ThriftBinaryProtocol extends TBinaryProtocol {
+
+  public ThriftBinaryProtocol(TTransport trans) {
+    super(trans);
+  }
+
+  private void checkElemType(byte type) throws TException {
+    // only valid types for an element in a container (List, Map, Set)
+    // are the ones that are considered in TProtocolUtil.skip()
+    switch (type) {
+      case TType.BOOL:
+      case TType.BYTE:
+      case TType.I16:
+      case TType.I32:
+      case TType.I64:
+      case TType.DOUBLE:
+      case TType.STRING:
+      case TType.STRUCT:
+      case TType.MAP:
+      case TType.SET:
+      case TType.LIST:
+        break;
+
+      // list other known types, but not expected
+      case TType.STOP:
+      case TType.VOID:
+      case TType.ENUM: // would be I32 on the wire
+      default:
+        throw new TException("Unexpected type " + type + " in a container");
+    }
+  }
+
+  @Override
+  public TMap readMapBegin() throws TException {
+    TMap map = super.readMapBegin();
+    checkElemType(map.keyType);
+    checkElemType(map.valueType);
+    return map;
+  }
+
+  @Override
+  public TList readListBegin() throws TException {
+    TList list = super.readListBegin();
+    checkElemType(list.elemType);
+    return list;
+  }
+
+  @Override
+  public TSet readSetBegin() throws TException {
+    TSet set = super.readSetBegin();
+    checkElemType(set.elemType);
+    return set;
+  }
+
+  public static class Factory implements TProtocolFactory {
+
+    public TProtocol getProtocol(TTransport trans) {
+      return new ThriftBinaryProtocol(trans);
+    }
+  }
+}
