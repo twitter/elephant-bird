5378c12cdb7d5d76511e45d9480754cb8f102762
Ruban Monu <rmonu@twitter.com>
2015-04-28 14:29:30 -0700

    working version for generic output format

--- core/src/main/java/com/twitter/elephantbird/mapreduce/io/GenericBlockWriter.java
+++ core/src/main/java/com/twitter/elephantbird/mapreduce/io/GenericBlockWriter.java
@@ -1,74 +1,16 @@
 package com.twitter.elephantbird.mapreduce.io;
 
-import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.google.protobuf.ByteString;
-import com.google.protobuf.Message;
-import com.twitter.elephantbird.util.Protobufs;
+import com.twitter.elephantbird.util.TypeRef;
 
-/**
- * A class to write blocks of serialized objects.
- */
-public class GenericBlockWriter extends BlockWriter<ByteString> {
+public class GenericBlockWriter<M> extends BinaryBlockWriter<M> {
 
-  private final OutputStream out_;
-  private final int numRecordsPerBlock_;
-  protected final Class<M> innerClass_;
-  private final BinaryConverter<M> binaryConverter_;
-  private int numRecordsWritten_ = 0;
-  private List<ByteString> protoBlobs_;
-
-  protected GenericBlockWriter(OutputStream out, Class<M> protoClass, BinaryConverter<M> binaryConverter, int numRecordsPerBlock) {
-    out_ = out;
-    numRecordsPerBlock_ = numRecordsPerBlock;
-    innerClass_ = protoClass;
-    binaryConverter_ = binaryConverter;
-    protoBlobs_ = new ArrayList<ByteString>(numRecordsPerBlock_);
+  public GenericBlockWriter(OutputStream out, BinaryConverter<M> conv, Class<M> recordClass) {
+    super(out, recordClass, conv, DEFAULT_NUM_RECORDS_PER_BLOCK);
   }
 
-  public void write(M message) throws IOException {
-    if (message instanceof Message) {
-      //a small hack to avoid extra copy, since we need a ByteString anyway.
-      protoBlobs_.add(((Message) message).toByteString());
-    } else {
-      protoBlobs_.add(ByteString.copyFrom(binaryConverter_.toBytes(message)));
-    }
-
-    numRecordsWritten_++;
-
-    if (protoBlobs_.size() == numRecordsPerBlock_) {
-      serialize();
-    }
+  public GenericBlockWriter(OutputStream out, BinaryConverter<M> conv, Class<M> recordClass, int numRecordsPerBlock) {
+    super(out, recordClass, conv, numRecordsPerBlock);
   }
-
-  public void finish() throws IOException {
-    if (protoBlobs_.size() > 0) {
-      serialize();
-    }
-  }
-
-  public void close() throws IOException {
-    finish();
-    out_.close();
-  }
-
-  protected void serialize() throws IOException {
-    out_.write(Protobufs.KNOWN_GOOD_POSITION_MARKER);
-    Message block = SerializedBlock
-        .newInstance(innerClass_.getCanonicalName(),protoBlobs_)
-        .getMessage();
-    protoBlobs_ = new ArrayList<ByteString>(numRecordsPerBlock_);
-    writeRawLittleEndian32(block.getSerializedSize());
-    block.writeTo(out_);
-  }
-
-  private void writeRawLittleEndian32(int size) throws IOException {
-     out_.write((size) & 0xFF);
-     out_.write((size >> 8) & 0xFF);
-     out_.write((size >> 16) & 0xFF);
-     out_.write((size >> 24) & 0xFF);
-   }
 }
--- core/src/main/java/com/twitter/elephantbird/mapreduce/output/LzoGenericBlockOutputFormat.java
+++ core/src/main/java/com/twitter/elephantbird/mapreduce/output/LzoGenericBlockOutputFormat.java
@@ -5,14 +5,48 @@ import java.io.IOException;
 import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 
-import com.twitter.elephantbird.mapreduce.io.RawBlockWriter;
-import com.twitter.elephantbird.mapreduce.io.RawBytesWritable;
+import com.twitter.elephantbird.mapreduce.input.BinaryConverterProvider;
+import com.twitter.elephantbird.mapreduce.io.BinaryConverter;
+import com.twitter.elephantbird.mapreduce.io.GenericBlockWriter;
+import com.twitter.elephantbird.mapreduce.io.GenericWritable;
+import com.twitter.elephantbird.util.HadoopCompat;
+import com.twitter.elephantbird.util.HadoopUtils;
 
-public class LzoGenericBlockOutputFormat extends LzoOutputFormat<byte[], GenericWritable> {
+import org.apache.hadoop.conf.Configuration;
+
+public class LzoGenericBlockOutputFormat<M> extends LzoOutputFormat<M, GenericWritable<M>> {
+
+  private static String CLASS_CONF_KEY = "elephantbird.class.for.LzoGenericBlockOutputFormat";
+  private static String GENERIC_ENCODER_KEY = "elephantbird.encoder.class.for.LzoGenericBlockOutputFormat";
+
+  public static void setGenericConverterClassConf(Class<?> clazz, Configuration conf) {
+    HadoopUtils.setClassConf(conf, GENERIC_ENCODER_KEY, clazz);
+  }
+
+  public static void setClassConf(Class<?> clazz, Configuration conf) {
+    HadoopUtils.setClassConf(conf, CLASS_CONF_KEY, clazz);
+  }
+
   @Override
-  public RecordWriter<byte[], RawBytesWritable> getRecordWriter(TaskAttemptContext job)
+  public RecordWriter<M, GenericWritable<M>> getRecordWriter(TaskAttemptContext job)
       throws IOException, InterruptedException {
-    return new LzoBinaryBlockRecordWriter<byte[], GenericWritable>(new RawBlockWriter(
-        getOutputStream(job)));
+    Configuration conf = HadoopCompat.getConfiguration(job);
+    Class<?> typeRef = null;
+    String genericEncoder = conf.get(GENERIC_ENCODER_KEY);
+    Class<?> encoderClazz = null;
+    BinaryConverterProvider<?> converterProvider = null;
+    try {
+      String typeRefClass = conf.get(CLASS_CONF_KEY);
+      typeRef = conf.getClassByName(typeRefClass);
+      encoderClazz = conf.getClassByName(genericEncoder);
+      converterProvider = (BinaryConverterProvider<?>)encoderClazz.newInstance();
+    } catch (Exception e) {
+      throw new RuntimeException("failed to instantiate class '" + genericEncoder + "'", e);
+    }
+
+    BinaryConverter<?> converter = converterProvider.getConverter(conf);
+
+    return new LzoBinaryBlockRecordWriter<M, GenericWritable<M>>(new GenericBlockWriter(
+        getOutputStream(job), converter, typeRef));
   }
 }
